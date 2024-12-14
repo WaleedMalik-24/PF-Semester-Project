a7a9c8ffa4d4d1d7a8dcabd7be1a797db27df0ae
Ayeshaazafarr <ayeshazafarr.0@gmail.com>
2024-12-15 01:07:27 +0500

    by ayesha

--- Airline Ticket System.cpp
+++ Airline Ticket System.cpp
@@ -6,4 +6,24 @@ int main() {
     //capacity of Seats
     int economySeats = 30;
     int businessSeats = 15;
+    int vipSeats = 5;
 
+    // Command input
+    string command;
+
+    cout << "Welcome to the Airline Reservation System!\n";
+    cout << "Total seats available: Economy (30), Business (15), VIP (5)\n";
+
+    while (true) {
+        // Display options
+        cout << "\nOptions:\n";
+        cout << "1. Enter 'book' to book a ticket.\n";
+        cout << "2. Enter 'cancel' to cancel a ticket.\n";
+        cout << "3. Enter 'status' to check seat availability.\n";
+        cout << "4. Enter 'stop' to stop the system.\n";
+        cout << "Your choice: ";
+        cin >> command;
+        
+        return 0;
+    }
+}
